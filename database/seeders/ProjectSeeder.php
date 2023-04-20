3ee790ac6e9f1c6f637169408f18355c3b6141ed
FedeAle1525 <fedeale.1525@gmail.com>
2023-04-20 16:03:59 +0200

    EDIT Seeder to insert data into Table "Projects"

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +15,20 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 50; $i++) {
+
+            $project = new Project();
+
+            $project->name = $faker->unique()->words($faker->numberBetween(2, 6), true);
+            // Grazie a una 'helper function' di Laravel posso generare slug da un metodo statico
+            $project->slug = Str::slug($project->name, '-');
+            $project->description = $faker->optional()->text(300);
+            $project->client = $faker->name($gender = 'male' | 'female');
+            $project->url = $faker->optional()->url();
+
+            $project->save();
+        }
     }
 }
