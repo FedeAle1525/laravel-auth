7c163a351918978ee59b1a4372da4ba6d998af6c
FedeAle1525 <fedeale.1525@gmail.com>
2023-04-20 15:29:25 +0200

    PHP create Table 'Projects' in Migration

--- database/migrations/2023_04_20_131120_create_projects_table.php
+++ database/migrations/2023_04_20_131120_create_projects_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 150)->unique();
+            // Campo Univoco generato dal Nome come riconoscimento al posto di ID
+            $table->string('slug');
+            $table->text('description')->nullable();
+            $table->string('client', 100);
+            $table->text('url')->nullable();
+            // Campo che serve per poter la gestione del recupero di elementi eliminati 
+            // [DA RICORDARE: importare Traits 'SoftDeletes' nel Model]
+            $table->softDeletes();
             $table->timestamps();
         });
     }
