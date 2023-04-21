8b099fd4c0908725ce9cf2417796d0689a5bb798
FedeAle1525 <fedeale.1525@gmail.com>
2023-04-21 17:45:36 +0200

    PHP implement CRUD 'Create'

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,1 +1,45 @@
-CREATE
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container text-center py-4">
+  <h1>Crea un Nuovo Progetto</h1>
+</div>
+
+<div class="container">
+  <form action="{{ route('projects.store') }}" method="post">
+    @csrf
+
+    <div class="mb-3">
+      <label class="form-label">Nome</label>
+      <div class="input-group">
+        <input type="text" class="form-control" name="title">
+      </div>
+    </div>
+
+    <div class="mb-3">
+      <label class="form-label">Descrizione</label>
+      <div class="input-group">
+        <textarea class="form-control" name="description" rows="6"></textarea>
+      </div>
+    </div>
+
+    <div class="mb-3">
+      <label class="form-label">Cliente</label>
+      <div class="input-group">
+        <input type="text" class="form-control" name="client">
+      </div>
+    </div>
+
+    <div class="mb-3">
+      <label class="form-label">URL</label>
+      <div class="input-group">
+        <input type="text" class="form-control" name="url">
+      </div>
+    </div>
+
+    <input type="submit" class="btn btn-primary" value="Crea">
+  </form>
+</div>
+
+@endsection
